fdd3094822e02946029f596ca6e6e80acfc18e54
lukaszmigdall-design <lukasz.migdall@gmail.com>
2026-02-21 22:40:48 +0100

    pierwsza wersja

--- Analiza motywów sekwencyjnch w DNA.py
+++ Analiza motywów sekwencyjnch w DNA.py
@@ -4,12 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 #program do analizy motywów DNA
-import re
-import requests
-from Bio import SeqID
+from Bio import SeqID, SeqIO
 from Bio import Entrez
-import matplotlib.pyplot as plt
-from collections import defaultdict
+
 
 class DNAmotyw:
     def __init__(self, sequence):
@@ -21,7 +18,19 @@ class DNAmotyw:
         record = SeqID.read(file_path, "fasta")
         return DNAmotyw(str(record.seq))
 
-    @staticmethod:
+    @staticmethod
+    def from_ncbi(accesion, email):
+        Entrez.email = email
+        handle = Entrez.efetch(db="nucleotide", id=accesion, rettype='fasta', retmode='text')
+        record = SeqIO.read(handle, "fasta")
+        record.close()
+        return DNAmotyw(str(record.seq))
+
+def main():
+    print(Analiza motywów sekwencjnych w DNA)
+    choice = input("Opcja 1 - wczytaj z pliku FASTA\nOpcja 2 - wczytaj z NCBI\nWybór(1 lub 2): ")
+    if choice == "1":
+        file_path =
 
 
 
